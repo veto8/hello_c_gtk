6a5cddf8f99330820801a287b1e8c73dee39f28f
veto <veto@myridia.com>
2025-03-05 19:05:41 +0700

    update

--- makewindows.sh
+++ makewindows.sh
@@ -1,2 +1,4 @@
-x86_64-w64-mingw32-gcc -o hello_64 hello.c `mingw64-pkg-config --cflags gtk+-3.0 --libs gtk+-3.0` -mwindows
+#!/bin/sh
 
+x86_64-w64-mingw32-gcc -o hello_64 hello.c `mingw64-pkg-config --cflags gtk+-3.0 --libs gtk+-3.0` -mwindows
+i686-w64-mingw32-gcc -o hello_32 hello.c `mingw64-pkg-config --cflags gtk+-3.0 --libs gtk+-3.0` -mwindows
